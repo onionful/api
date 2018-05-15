0c2fc04db3d8ff365adf2f0a63b40d5409f46ad3
hsz <jakub@chrzanowski.info>
2018-05-16 00:49:20 +0200

    fetch users with limited fields

--- src/users.js
+++ src/users.js
@@ -1,6 +1,8 @@
 import { wrapper } from './utils';
 
-const list = (params, { Auth0 }) => Auth0.getUsers();
+const list = (params, { Auth0 }) => Auth0.getUsers({
+  fields: 'email,name,nickname,picture,created_at,last_login,logins_count',
+});
 
 module.exports = {
   list: wrapper(list, { withAuth0: true }),
