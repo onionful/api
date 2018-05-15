26df613abfe203343c0ef8f88aa920a6108f8dc3
hsz <jakub@chrzanowski.info>
2018-05-15 22:50:42 +0200

    auth rotateToken action; code cleanup

--- src/auth.js
+++ src/auth.js
@@ -1,58 +1,74 @@
+import AWS from 'aws-sdk';
+import axios from 'axios';
 import jwt from 'jsonwebtoken';
 import jwksClient from 'jwks-rsa';
-import { errors } from './utils';
+import { errors, wrapper } from './utils';
 
-const AUTH0_DOMAIN = '...';
-const AUTH0_CLIENT_ID = '...';
-const JWKS_URI = '...';
+const { STAGE } = process.env;
+const wrapperOptions = { secretName: `${STAGE}/onionful`, raw: true };
 
-const generatePolicy = (principalId, effect, resource) => ({
+const generatePolicy = (principalId, Effect, Resource) => ({
   principalId,
-  policyDocument: effect && resource ? {
+  policyDocument: Effect && Resource ? {
     Version: '2012-10-17',
-    Statement: [{
-      Action: 'execute-api:Invoke',
-      Effect: effect,
-      Resource: resource,
-    }],
+    Statement: [{ Action: 'execute-api:Invoke', Effect, Resource }],
   } : null,
 });
 
-const client = jwksClient({
-  cache: true,
-  rateLimit: true,
-  jwksRequestsPerMinute: 10,
-  jwksUri: JWKS_URI,
-});
-
-const check = ({ authorizationToken, methodArn }, context, callback) => {
+const check = ({ authorizationToken, methodArn }, { config }) => new Promise((resolve, reject) => {
   const [bearer, token] = (authorizationToken || '').split(' ');
   if (!(bearer.toLowerCase() === 'bearer' && token)) {
-    callback(new errors.Unauthorized());
+    console.error('Invalid token', { bearer, token });
+    reject(new errors.Unauthorized());
   }
 
-  const { header: { kid }} = jwt.decode(token, { complete: true });
-  client.getSigningKey(kid, function (err, { publicKey, rsaPublicKey }) {
+  const { header: { kid } } = jwt.decode(token, { complete: true });
+
+  jwksClient({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10,
+    jwksUri: config.auth0.jwksUri,
+  }).getSigningKey(kid, (err, { publicKey, rsaPublicKey } = {}) => {
     if (err) {
-      callback(new errors.Unauthorized());
+      console.error('JWKS error', err);
+      reject(new errors.Unauthorized());
     }
 
     try {
       jwt.verify(token, publicKey || rsaPublicKey, {
-        audience: AUTH0_CLIENT_ID,
-        issuer: `https://${AUTH0_DOMAIN}/`,
-      }, (verifyError, { sub }) => {
+        audience: config.auth0.clientId,
+        issuer: `https://${config.auth0.domain}/`,
+      }, (verifyError, { sub } = {}) => {
         if (verifyError) {
-          callback(new errors.Unauthorized());
+          console.error('VerifyError', verifyError);
+          reject(new errors.Unauthorized());
         }
-        callback(null, generatePolicy(sub, 'Allow', methodArn));
-      })
+
+        resolve(generatePolicy(sub, 'Allow', methodArn));
+      });
     } catch (e) {
-      callback(new errors.Unauthorized());
+      console.error('JWT error', e);
+      reject(new errors.Unauthorized());
     }
   });
-};
+});
+
+const rotateToken = (params, { config: { auth0 } }) =>
+  axios.post(`https://${auth0.domain}/oauth/token`, {
+    client_id: auth0.api.clientId,
+    client_secret: auth0.api.clientSecret,
+    audience: `https://${auth0.domain}/api/v2/`,
+    grant_type: 'client_credentials',
+  }, { headers: { 'content-type': 'application/json' } })
+    .then(({ data: { access_token } }) =>
+      new Promise((resolve, reject) => new AWS.SecretsManager().putSecretValue({
+        SecretId: `${STAGE}/onionful/token`,
+        SecretString: access_token,
+      }, (error, data) => (error ? reject(error) : resolve(data))))
+    );
 
 module.exports = {
-  check: check,
+  check: wrapper(check, wrapperOptions),
+  rotateToken: wrapper(rotateToken, wrapperOptions),
 };
--- src/utils/wrapper.js
+++ src/utils/wrapper.js
@@ -2,15 +2,21 @@ import middy from 'middy';
 import secrets from 'middy-secrets';
 import { cors, httpErrorHandler, jsonBodyParser, urlEncodeBodyParser } from 'middy/middlewares';
 
-export default (fn, { secretName } = {}) => {
-  const handler = middy((...args) => Promise.resolve(fn(...args)).then(data => ({ body: data })))
+export default (fn, { secretName, raw = false } = {}) => {
+  const handler = middy((...args) =>
+    Promise.resolve(fn(...args)).then(data => (raw ? data : { body: data }))
+  )
     .use(cors())
     .use(jsonBodyParser())
     .use(urlEncodeBodyParser())
     .use(httpErrorHandler());
 
   if (secretName) {
-    handler.use(secrets({ secretName }));
+    handler.use(secrets({
+      secretName,
+      setToContext: true,
+      contextKey: 'config',
+    }));
   }
 
   return handler;
