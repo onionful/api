903a12f94a4a14cff8d8ccccdc76a62e2908eda0
hsz <jakub@chrzanowski.info>
2018-08-18 11:30:48 +0200

    cache-loader + thread-loader for better webpack performance

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,24 @@ const webpack = require('webpack');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  devtool: 'nosources-source-map',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   module: {
-    rules: [{
-      test: /\.js$/,
-      loaders: ['babel-loader', 'eslint-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: [
+          { loader: 'cache-loader' },
+          { loader: 'thread-loader', options: { workers: require('os').cpus().length - 1 } },
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
   },
-  plugins: [
-    new webpack.DefinePlugin({ 'global.GENTLY': false }),
-  ],
+  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
   resolve: {
     alias: {
       deepmerge$: path.resolve(__dirname, './node_modules/deepmerge/dist/umd.js'),
