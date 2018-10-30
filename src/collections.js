2de7a11f4068216fca2cc59da89813e4aba293b4
hsz <jakub@chrzanowski.info>
2018-10-30 07:01:46 +0100

    collections delete, update with possibility to change collection id

--- src/collections.js
+++ src/collections.js
@@ -1,27 +1,42 @@
-import { sortBy } from 'lodash';
+import { kebabCase, sortBy } from 'lodash';
 import { Collection } from './models';
-import { errors, verify, wrapper } from './utils';
+import { verify, wrapper } from './utils';
 
 const parse = ({ fields, ...content }) => ({
   fields: sortBy(fields || [], 'order').map(({ order, ...field }) => field),
   ...content,
 });
 
-export const create = wrapper(({ headers: { Space: space }, body }) =>
-  Collection.create({ ...body, space }).then(parse),
-);
+export const create = wrapper(({ headers: { Space: space }, body }) => {
+  const id = kebabCase(body.id || body.name);
 
-export const update = wrapper(
-  ({ headers: { Space: space }, body: { fields, ...body }, pathParameters: { id } }) =>
-    Collection.update(
-      { space, id },
-      { fields: fields.map((field, order) => ({ ...field, order })), ...body },
-      { condition: 'attribute_exists(id)' },
-    )
-      .then(parse)
-      .catch(({ message }) => {
-        throw new errors.NotFound(message);
-      }),
+  return Collection.get({ space, id })
+    .then(verify.conflict)
+    .then(() => Collection.create({ ...body, id, space }).then(parse));
+});
+
+export const update = wrapper(({ headers: { Space: space }, body, pathParameters: { id } }) =>
+  Collection.get({ space, id })
+    .then(verify.presence)
+    .then(previous => {
+      const fields = (body.fields || previous.fields).map((field, order) => ({ ...field, order }));
+
+      const newId = kebabCase(body.id);
+      if (!newId || id === newId) {
+        // regular update, no id changed
+        return Collection.update(
+          { space, id },
+          { ...body, fields },
+          { condition: 'attribute_exists(id)' },
+        );
+      }
+
+      return Collection.get({ space, id: newId })
+        .then(verify.conflict)
+        .then(() => Collection.create({ ...previous, ...body, fields, space, id: newId }))
+        .then(collection => Collection.delete({ id, space }).then(() => collection));
+    })
+    .then(parse),
 );
 
 export const get = wrapper(({ headers: { Space: space }, pathParameters: { id } }) =>
@@ -35,4 +50,8 @@ export const list = wrapper(({ headers: { Space: space } }) =>
     .eq(space)
     .exec()
     .then(items => items.map(parse)),
+);
+
+export const remove = wrapper(({ headers: { Space: space }, pathParameters: { id } }) =>
+  Collection.delete({ space, id }).then(parse),
 );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,48 @@
 import errors from 'http-errors';
+import { isEmpty } from 'lodash';
 
 export { default as db } from './db';
 export { default as wrapper } from './wrapper';
 export { errors };
 
+/**
+ * Creates a verifier that throws given error if condition is met.
+ *
+ * @example
+ * const verifier = verifier({
+ *   error = errors.NotFound,
+ *   message = errors.NotFound.message,
+ *   condition = item => !item,
+ * })
+ *
+ * @param error
+ * @param message
+ * @param condition
+ * @returns {function(*=): *}
+ */
+export const verifier = ({ error, message, condition }) => item => {
+  if (condition(item)) {
+    throw error(message);
+  }
+  return item;
+};
+
+/**
+ * Common verifiers - checks initialized using {@link verifier}.
+ *
+ * @type {{presence: (function(*=): *), conflict: (function(*=): *)}}
+ */
 export const verify = {
-  presence: data => {
-    if (!data) {
-      throw new errors.NotFound();
-    }
-    return data;
-  },
+  presence: verifier({
+    error: errors.NotFound,
+    message: errors.NotFound.message,
+    condition: item => isEmpty(item),
+  }),
+  conflict: verifier({
+    error: errors.Conflict,
+    message: errors.Conflict.message,
+    condition: item => item,
+  }),
 };
 
 export const userFields = [
