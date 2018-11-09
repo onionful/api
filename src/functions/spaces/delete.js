f30f12e236ce957aee39e0905015c9bf625551a4
hsz <jakub@chrzanowski.info>
2018-11-09 19:55:04 +0100

    spaces.delete

--- src/functions/spaces/delete.js
+++ src/functions/spaces/delete.js
@@ -1,17 +1,8 @@
-import { snakeCase } from 'lodash';
 import { Space } from 'models';
-import { errors, verify, wrapper } from 'utils';
+import { errors, wrapper } from 'utils';
 
-export const create = wrapper(({ body: { name, ...rest } }) =>
-  Space.create({ ...rest, id: snakeCase(name), name }),
-);
-
-export const update = wrapper(({ body, pathParameters: { id } }) =>
-  Space.update({ id }, body, { condition: 'attribute_exists(id)' }).catch(({ message }) => {
+export default wrapper(({ pathParameters: { id } }) =>
+  Space.delete({ id }, { condition: 'attribute_exists(id)' }).catch(({ message }) => {
     throw new errors.NotFound(message);
   }),
 );
-
-export const get = wrapper(({ pathParameters: { id } }) => Space.get({ id }).then(verify.presence));
-
-export const list = wrapper(() => Space.scan().exec());
